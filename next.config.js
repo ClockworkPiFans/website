f37cdecde6eb46f1089708085015f4723167c38a
Lazy_V <33739170+zzxzzk115@users.noreply.github.com>
2024-12-13 17:37:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const repo = 'website'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
-    path: '/',
+    path: assetPrefix,
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
